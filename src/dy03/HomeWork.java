4bd553cc623060e05cdd03b798e8792b70b269a2
844169374 <844169374@qq.com>
2020-06-17 21:41:13 +0800

    等腰三角形

--- src/dy03/HomeWork.java
+++ src/dy03/HomeWork.java
@@ -1,5 +1,19 @@
 package dy03;
 
 public class HomeWork {
-}
-//打印99乘法表
+
+        public static void main(String[] args) {
+            //根据给定的方法来输出一个等腰三角
+            int max = 5;
+            //控制行数
+            for(int i = 1; i<=5 ; i++) {
+                //控制空格
+                for (int j = 1; j <= max - i; j++)
+                    System.out.print(" ");
+                //控制星数
+                for (int k = 1; k <= i * 2 - 1; k++)
+                    System.out.print("*");
+                System.out.println();
+            }
+        }
+    }
